3c0eae4689667f04cabdc6d75a638d7b94368345
NicolasCopernico <c.bastos100@gmail.com>
2022-10-17 21:07:18 -0300

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:peliculas/src/pages/home_page.dart';
 import 'package:peliculas/src/pages/pelicula_detalle.dart';
 
+//esto es un comentario
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
